1f3e70afe8a1b2776ff6989f88450f24c4b33356
rishik840 <rhul840@gmail.com>
2015-10-21 12:47:56 -0600

    changes made

---
+++ Mandala/Assets/MenuOptions.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuOptions : MonoBehaviour {
+    public float theta_scale = 0.01f;        //Set lower to add more points
+    public int size; //Total number of points in circle
+    public float radius = 3f;
+    LineRenderer lineRenderer;
+    public Color c1 = Color.black;
+    public Color c2 = new Color(1, 1, 1, 0);
+    public GameObject canvasSheet;
+
+    public void DrawCircle()
+    {
+        float sizeValue = (2.0f * Mathf.PI) / theta_scale;
+        size = (int)sizeValue;
+        size++;
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        //lineRenderer.material = new Material(Shader.Find("Materials/Menu"));
+        lineRenderer.SetWidth(0.02f, 0.02f); //thickness of line
+        lineRenderer.SetVertexCount(size);
+
+
+        Vector3 pos;
+        float theta = 0f;
+        for (int i = 0; i < size; i++)
+        {
+            theta += (2.0f * Mathf.PI * theta_scale);
+            float x = radius * Mathf.Cos(theta);
+            float y = radius * Mathf.Sin(theta);
+            x += canvasSheet.transform.position.x;
+            y += canvasSheet.transform.position.y;
+            float z = canvasSheet.transform.position.z - 0.1f;
+            pos = new Vector3(x, y, z);
+            lineRenderer.SetColors(c1, c1);
+            lineRenderer.SetPosition(i, pos);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
---
+++ Mandala/Assets/Scripts/MenuSelect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSelect : MonoBehaviour {
+
+    bool isClicked=false;
+    public GameObject menu;
+    void OnMouseDown()
+    {
+        if (!isClicked)
+        {
+            isClicked = true;
+            Instantiate(menu);
+        }
+        else
+        {
+            isClicked = false;
+            Destroy(menu);
+        }        
+    }
+
+}
